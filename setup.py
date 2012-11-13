9d038f18ef165e8b6e5f4f29343e336cf4468126
Jan Safranek <jsafrane@redhat.com>
2012-11-13 09:11:34 +0100

    Relicense to LGPLv2+

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ setup(
     author='Jan Safranek',
     author_email='jsafrane@redhat.com',
     url='https://fedorahosted.org/openlmi/',
-    version='0.4.0',
+    version='0.4.1',
     package_dir={'openlmi.storage': 'providers'},
     packages=['openlmi.storage', 'openlmi.storage.wrapper', 'openlmi.storage.util'],
     classifiers=[
-        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
         'Operating System :: POSIX :: Linux',
         'Topic :: System :: Systems Administration',
         ]
