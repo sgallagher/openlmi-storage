ae86393fe9d19ea88b2ed035df70450c9ba6c395
Jan Safranek <jsafrane@redhat.com>
2012-11-21 12:14:31 +0100

    Implemented GetSupportedSizeRange and GetSupportedSizes methods.

--- providers/LMI_VGStoragePool.py
+++ providers/LMI_VGStoragePool.py
@@ -140,4 +140,125 @@ class LMI_VGStoragePool(DeviceProvider):
             else:
                 yield self.get_instance(env, model, device)
 
+    def cim_method_getsupportedsizes(self, env, object_name,
+                                     param_elementtype=None,
+                                     param_goal=None,
+                                     param_sizes=None):
+        """Implements LMI_VGStoragePool.GetSupportedSizes() """
+        logger = env.get_logger()
+        logger.log_debug('Entering %s.cim_method_getsupportedsizes()' \
+                % self.__class__.__name__)
+        rval = self.VGStoragePoolValues.GetSupportedSizes.Use_GetSupportedSizes_instead
+        return (rval, [])
+
+    def cim_method_getsupportedsizerange(self, env, object_name,
+                                         param_minimumvolumesize=None,
+                                         param_maximumvolumesize=None,
+                                         param_elementtype=None,
+                                         param_volumesizedivisor=None,
+                                         param_goal=None):
+        """Implements LMI_VGStoragePool.GetSupportedSizeRange()
+        param_minimumvolumesize --  The input parameter MinimumVolumeSize (type pywbem.Uint64) 
+            The minimum size for a volume/pool in bytes.
             
+        param_maximumvolumesize --  The input parameter MaximumVolumeSize (type pywbem.Uint64) 
+            The maximum size for a volume/pool in bytes.
+            
+        param_elementtype --  The input parameter ElementType (type pywbem.Uint16 self.Values.GetSupportedSizeRange.ElementType) 
+            The type of element for which supported size ranges are
+            reported. The Thin Provision values are only supported when
+            the Thin Provisioning Profile is supported; the resulting
+            StorageVolues/LogicalDisk shall have ThinlyPprovisioned set to
+            true.
+            
+        param_volumesizedivisor --  The input parameter VolumeSizeDivisor (type pywbem.Uint64) 
+            A volume/pool size must be a multiple of this value which is
+            specified in bytes.
+            
+        param_goal --  The input parameter Goal (type REF (pywbem.CIMInstanceName(classname='CIM_StorageSetting', ...)) 
+            The StorageSetting for which supported size ranges should be
+            reported for.
+
+        Returns a two-tuple containing the return value (type pywbem.Uint32 self.Values.GetSupportedSizeRange)
+        and a list of CIMParameter objects representing the output parameters
+
+        Output parameters:
+        MinimumVolumeSize -- (type pywbem.Uint64) 
+            The minimum size for a volume/pool in bytes.
+            
+        MaximumVolumeSize -- (type pywbem.Uint64) 
+            The maximum size for a volume/pool in bytes.
+            
+        VolumeSizeDivisor -- (type pywbem.Uint64) 
+            A volume/pool size must be a multiple of this value which is
+            specified in bytes.
+            
+        Possible Errors:
+        CIM_ERR_ACCESS_DENIED
+        CIM_ERR_INVALID_PARAMETER (including missing, duplicate, 
+            unrecognized or otherwise incorrect parameters)
+        CIM_ERR_NOT_FOUND (the target CIM Class or instance does not 
+            exist in the specified namespace)
+        CIM_ERR_METHOD_NOT_AVAILABLE (the CIM Server is unable to honor 
+            the invocation request)
+        CIM_ERR_FAILED (some other unspecified error occurred)
+
+        """
+
+        logger = env.get_logger()
+        logger.log_debug('Entering %s.cim_method_getsupportedsizerange()' \
+                % self.__class__.__name__)
+
+        if not self.providesName(object_name):
+            raise pywbem.CIMError(pywbem.CIM_ERR_NOT_FOUND, "Wrong keys.")
+        device = self.getDeviceForName(object_name)
+        if not device:
+            raise pywbem.CIMError(pywbem.CIM_ERR_NOT_FOUND, "Cannot find the VG.")
+        
+        # we support only logical disks for now (should be StorageExtent)
+        if (param_elementtype
+                and param_elementtype != self.VGStoragePoolValues.GetSupportedSizeRange.ElementType.Logical_Disk):
+            ret = self.VGStoragePoolValues.GetSupportedSizeRange.Invalid_Element_Type
+            return (ret, [])
+        
+        # TODO: check Goal setting!
+
+        extentSize = long(device.peSize * MEGABYTE)
+        availableSize = long(device.peSize * device.freeExtents * MEGABYTE)
+
+        out_params = []
+        out_params+= [pywbem.CIMParameter('minimumvolumesize', type='uint64',
+                           value=pywbem.Uint64(extentSize))]
+        out_params+= [pywbem.CIMParameter('maximumvolumesize', type='uint64',
+                           value=pywbem.Uint64(availableSize))]
+        out_params+= [pywbem.CIMParameter('volumesizedivisor', type='uint64',
+                           value=pywbem.Uint64(extentSize))]
+        rval = pywbem.Uint32(self.VGStoragePoolValues.GetSupportedSizeRange.Method_completed_OK)
+        return (rval, out_params)
+
+
+
+    class VGStoragePoolValues:
+        class GetSupportedSizeRange(object):
+            Method_completed_OK = pywbem.Uint32(0)
+            Method_not_supported = pywbem.Uint32(1)
+            Use_GetSupportedSizes_instead = pywbem.Uint32(2)
+            Invalid_Element_Type = pywbem.Uint32(3)
+            class ElementType(object):
+                Storage_Pool = pywbem.Uint16(2)
+                Storage_Volume = pywbem.Uint16(3)
+                Logical_Disk = pywbem.Uint16(4)
+                Thin_Provisioned_Volume = pywbem.Uint16(5)
+                Thin_Provisioned_Logical_Disk = pywbem.Uint16(6)
+        
+        class GetSupportedSizes(object):
+            Method_completed_OK = pywbem.Uint32(0)
+            Method_not_supported = pywbem.Uint32(1)
+            Use_GetSupportedSizes_instead = pywbem.Uint32(2)
+            Invalid_Element_Type = pywbem.Uint32(3)
+            class ElementType(object):
+                Storage_Pool = pywbem.Uint16(2)
+                Storage_Volume = pywbem.Uint16(3)
+                Logical_Disk = pywbem.Uint16(4)
+                Thin_Provisioned_Volume = pywbem.Uint16(5)
+                Thin_Provisioned_Logical_Disk = pywbem.Uint16(6)
