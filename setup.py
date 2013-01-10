279d1a7d2a25e229a98f640c6bc84c06997a2b44
Jan Safranek <jsafrane@redhat.com>
2013-01-10 12:49:21 +0100

    Update the setup to the new model.

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     author='Jan Safranek',
     author_email='jsafrane@redhat.com',
     url='https://fedorahosted.org/openlmi/',
-    version='0.4.1',
+    version='0.5.0',
     package_dir={'openlmi.storage': 'providers'},
-    packages=['openlmi.storage', 'openlmi.storage.wrapper', 'openlmi.storage.util'],
+    packages=['openlmi.storage', 'openlmi.storage.util'],
     classifiers=[
         'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
         'Operating System :: POSIX :: Linux',
